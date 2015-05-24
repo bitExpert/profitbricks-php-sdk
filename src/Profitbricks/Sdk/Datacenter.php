8344b49eb26de66055d47b8aa9e563b0264869c9
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2015-05-24 21:11:40 +0200

    Reformatted code.

--- src/Profitbricks/Sdk/Datacenter.php
+++ src/Profitbricks/Sdk/Datacenter.php
@@ -15,54 +15,54 @@ namespace Profitbricks\Sdk;
  */
 class Datacenter
 {
-    /**
-     * @var string
-     */
-    protected $id;
-    /**
-     * @var string
-     */
-    protected $name;
-    /**
-     * @var string
-     */
-    protected $version;
+  /**
+   * @var string
+   */
+  protected $id;
+  /**
+   * @var string
+   */
+  protected $name;
+  /**
+   * @var string
+   */
+  protected $version;
 
-    /**
-     * Creates a new {@link \Profitbricks\Sdk\Datacenter},
-     *
-     * @param string $id
-     * @param string $name
-     * @param string $version
-     */
-    public function __construct($id, $name, $version)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->version = $version;
-    }
+  /**
+   * Creates a new {@link \Profitbricks\Sdk\Datacenter},
+   *
+   * @param string $id
+   * @param string $name
+   * @param string $version
+   */
+  public function __construct($id, $name, $version)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->version = $version;
+  }
 
-    /**
-     * @return string
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * @return string
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
 
-    /**
-     * @return string
-     */
-    public function getVersion()
-    {
-        return $this->version;
-    }
+  /**
+   * @return string
+   */
+  public function getVersion()
+  {
+    return $this->version;
+  }
 }
